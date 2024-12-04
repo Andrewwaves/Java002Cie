3de1513176f3b309331105f121ae9da80a028f23
cie <cie@DESKTOP-U2G3M5P>
2024-12-04 13:37:59 +0100

    Math

--- Pruebas01/src/Pruebas01/EjercicioCalculadora02Mejorada.java
+++ Pruebas01/src/Pruebas01/EjercicioCalculadora02Mejorada.java
@@ -7,9 +7,17 @@ public class EjercicioCalculadora02Mejorada {
 	public static void main(String[] args) {
 		//CalculadoraMejorada c=new CalculadoraMejorada();
 	
+		//Math. algo para hacer directamente los calculo a traves de este funcion.
+	System.out.println(Math.pow(5, 2));
+	
 		//metodo estatico, invocas a la funcion directamente por la clase
 		System.out.println(CalculadoraMejorada.sumar(2,2));
 
+		System.out.println(CalculadoraMejorada.restar(2,2));
+		
+		System.out.println(CalculadoraMejorada.multiplicar(2,2));
+		
+		System.out.println(CalculadoraMejorada.dividir(2,2));
 	}
 
 }
