f99d668de28dff857b1f84c4dc0b57ece3da106f
Andrewwaves <andreacanalesmartin2000@gmail.com>
2024-12-17 13:35:25 +0100

    Add files via upload

--- Pruebas002/src/SemanaLunes16/Principal6.java
+++ Pruebas002/src/SemanaLunes16/Principal6.java
@@ -5,8 +5,11 @@ public class Principal6 {
 		
 		PersonaRepositoryNueva repo= new PersonaRepositoryNueva();
 		
-		Persona p1=new Persona("juan","gomez",60);
-		repo.insertar(p1);
+		//Persona p1=new Persona("juan","gomez",60);
+		//repo.insertar(p1);
+		
+		Persona p2=new Persona("manuel");
+		repo.quitar(p2);
 	}
 		
 }
