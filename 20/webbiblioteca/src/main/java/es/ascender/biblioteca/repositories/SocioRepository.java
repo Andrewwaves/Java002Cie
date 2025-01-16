5c13866ce680722baf14e726887f5659606674f5
Andrewwaves <andreacanalesmartin2000@gmail.com>
2025-01-16 13:43:19 +0100

    Add files via upload
    EJEMPLO DE ACCESO A LA BASE DE DATOS DESDE UN REPOSITORIO [FUNCIONA]

---
+++ 20/webbiblioteca/src/main/java/es/ascender/biblioteca/repositories/SocioRepository.java
@@ -0,0 +1,10 @@
+package es.ascender.biblioteca.repositories;
+
+import java.util.List;
+
+import es.ascender.biblioteca.negocio.Socio;
+
+public interface SocioRepository {
+
+	List<Socio> buscarTodos();
+}
